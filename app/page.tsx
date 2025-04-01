9496eab0fb3f9ac8ed0e6bc1de174915b40b4e88
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2025-04-01 15:54:42 +0530

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center text-4xl text-slate-600 shadow-sm-slate/15 '>
+      Delta Coming soon ...
+    </div>
+  );
+}
